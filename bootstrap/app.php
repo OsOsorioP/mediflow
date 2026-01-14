3c40b86d98219a71f0aeecc6974cfb33c2ac9ddd
Oscar Osorio <oscar.osorio1884@gmail.com>
2026-01-14 15:16:03 -0500

    feat: register EnsureTenantContext middleware for authenticated web routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureTenantContext;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         // Aplicar a todas las rutas web autenticadas
         $middleware->appendToGroup('web', [
-            // El middleware 'tenant' se aplicará después de 'auth'
+            'tenant' => EnsureTenantContext::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
