922575bbe4ae7f9630ecaa975c789fbaae738541
Oscar Osorio <oscar.osorio1884@gmail.com>
2026-01-18 07:53:44 -0500

    feat(landing): update landing page design and global styles

---
+++ resources/views/livewire/welcome/navLink.blade.php
@@ -0,0 +1,17 @@
+<nav class="px-4 flex gap-4">
+
+    <a href="{{ url('#features') }}" class="">
+        Características
+    </a>
+
+    <a href="{{ url('#solutions') }}" class="">
+        Soluciones
+    </a>
+
+    <a href="{{ url('#pricing') }}" class="">
+        Precios
+    </a>
+    <a href="{{ url('#about') }}" class="">
+        Nosotros
+    </a>
+</nav>
