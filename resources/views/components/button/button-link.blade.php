9023386c1caba93334e44c94429b2dfdc286f5e1
Oscar Osorio <oscar.osorio1884@gmail.com>
2026-01-21 09:49:05 -0500

    refactor(ui): modernize standard blade components and layout

---
+++ resources/views/components/button/button-link.blade.php
@@ -0,0 +1,5 @@
+<a href="{{ $href }}"
+    class="inline-flex items-center px-5 py-2.5 border border-transparent rounded-xl shadow-lg text-sm font-semibold text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-200 transition-all duration-300 transform hover:-translate-y-0.5">
+    {!! $icon !!}
+    {{ $text }}
+</a>
